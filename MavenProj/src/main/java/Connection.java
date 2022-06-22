c9921a785c8946b978464fcdbd0b8726a010fdb6
MaratGiz <marat.gizatullin6605@gmail.com>
2022-06-22 17:56:49 +0300

    Renamed main file to Connection
    Also created the basic code for linking with the users.yml file

---
+++ MavenProj/src/main/java/Connection.java
@@ -0,0 +1,14 @@
+
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.File;
+
+public class Connection {
+	public static void main(String[] args) {
+		
+//		InputStream inputStream = new FileInputStream(new File("users.yml"));
+//		Yaml yaml = new Yaml();
+//		Map data = yaml.load(inputStream);
+//		System.out.println(data);
+	}
+}
