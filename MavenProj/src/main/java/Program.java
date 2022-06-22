290331b3d5dfe2623d161e303d49c8121860137b
MaratGiz <marat.gizatullin6605@gmail.com>
2022-06-22 16:57:45 +0300

    Recreated Maven project in right folder

---
+++ MavenProj/src/main/java/Program.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Program {
+	public static void main(String[] args) {
+		FileInputStream inputStream = new FileInputStream(new File("student.yml")); 
+		
+	}
+}
--- test.java
+++
@@ -1,8 +0,0 @@
-
-//import Java.util.Scanner;
-
-public class test {
-	public static void main (String[] args) {
-		System.out.println("Test text");
-	}
-}
