9bfe1006e60cd0e1bcfb2ad649965ddb5c055b04
MaratGiz <38915137+MaratGiz@users.noreply.github.com>
2022-06-24 11:57:28 +0300

    Update Connection.java

--- MavenProj/src/main/java/Connection.java
+++ MavenProj/src/main/java/Connection.java
@@ -1,14 +1,39 @@
 
+//import java.util.Scanner;
+import java.util.Map;
+import java.io.File;
 import java.io.InputStream;
 import java.io.FileInputStream;
-import java.io.File;
+import java.io.FileNotFoundException;
+import org.yaml.snakeyaml.Yaml;
+//import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
 
 public class Connection {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-//		InputStream inputStream = new FileInputStream(new File("users.yml"));
-//		Yaml yaml = new Yaml();
-//		Map data = yaml.load(inputStream);
-//		System.out.println(data);
+		org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Connection.class);
+	    logger.trace("hello");
+	    
+//	    BasicConfigurator.configure();
+	    
+	    PropertyConfigurator.configure("/home/student/Practice/MavenProj/log4j.properties");
+	    
+		String path = args[0];
+		
+
+				
+// 		/home/student/Practice/MavenProj/users.yml
+		InputStream inputStream = new FileInputStream(new File(path));
+		
+		Yaml yaml = new Yaml();
+		Map<String, Object> data = yaml.load(inputStream); 
+		
+		System.out.println(data);
+		
+		
 	}
 }
